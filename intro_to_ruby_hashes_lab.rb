ee913273091c0c85f390798b19d888a0234126a3
Julian Pormentilla <julian.pormentilla@gmail.com>
2020-02-25 05:14:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -55,7 +55,7 @@ def monopoly_with_third_tier
 	          :reading_railroad => {},
             :pennsylvania_railroad => {},
             :b_and_o_railroad => {},
-            :shortline => {}
+            :shortline_railroad => {}
 	        }  
 	  
 	    }
